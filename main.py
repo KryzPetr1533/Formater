a091a35352d733a21e33fb59179c8927d8fe2cd0
KryzPetr1533 <62902540+KryzPetr1533@users.noreply.github.com>
2022-04-10 18:07:42 +0300

    formatter to exel first version

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import xlsxwriter
+
+# Create a workbook (exel file) and add a worksheet (table).
+workbook = xlsxwriter.Workbook('Test.xlsx')
+worksheet = workbook.add_worksheet()
+
+# Some data (this is test version) we want to write to the worksheet.
+response = requests.get('http://catalog.data.gov/api/3/action/package_list')
+response = response.json()
+
+# print(*response.items(), sep = '\n')
+# Start from the first cell. Rows and columns are zero indexed.
+row = 0
+col = 0
+width = 20
+
+for items in response['result']['results']:
+    col = 0
+    for name, data in items.items():
+        if type(data) is not list and type(data) is not dict and data is not None:
+            if row == 0:
+                worksheet.write(row, col, name, workbook.add_format({'bold': 1}))
+            else:
+                worksheet.write(row, col, data)
+            # Adjust the column width.
+            worksheet.set_column(row, col, width)
+            col += 1
+    row += 1
+
+# Iterate over the data and write it out row by row.
+
+# # Write a total using a formula.
+# worksheet.write(row, 0, 'Total')
+# worksheet.write(row, 1, '=SUM(B1:B4)')
+
+workbook.close()
