fd71eacfb814d5fffeffc1ca9554ea7ed9a68cb5
Stanislav <53866520+stasray@users.noreply.github.com>
2022-05-20 11:12:36 +0300

    Added MySQL Export type

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 import xlsxwriter
 import csv
+import mysql.connector
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 import os
+
+from mysql.connector import Error
 
 Form, Window = uic.loadUiType("gui.ui")
 app = QApplication([])
@@ -68,6 +71,57 @@ def continue_click():
                 worksheet.writerow(string)
                 row += 1
         print("Successful exported data from " + form.lineEdit.text() + " to CSV .csv file!")
+    if form.comboBox.currentText() == "MySQL":
+        with open(name + '.csv', 'w', newline='') as csvfile:
+
+            mysql_host = "localhost"
+            mysql_database = "test"
+            mysql_port = 3306
+            mysql_username = "root"
+            mysql_password = "adminadmin"
+            mysql_table = "result"
+
+            try:
+                connection = mysql.connector.connect(
+                    host=mysql_host,
+                    port=mysql_port,
+                    database=mysql_database,
+                    user=mysql_username,
+                    password=mysql_password)
+                if connection.is_connected():
+                    cursor = connection.cursor()
+                    sql_delete = 'DROP TABLE IF EXISTS ' + mysql_table
+                    cursor.execute(sql_delete)
+
+                    row = 0
+                    count_args = 0
+                    sql_create = 'CREATE TABLE ' + mysql_table + ' ('
+                    sql_list = []
+                    for items in response['result']['results']:
+                        sql_newline = 'INSERT ' + mysql_table + ' VALUES ('
+                        sql_value = []
+                        for name, data in items.items():
+                            if type(data) is not list and type(data) is not dict and data is not None:
+                                if row == 0:
+                                    sql_create += name + ' TEXT,'
+                                    count_args += 1
+                                else:
+                                    sql_newline += '%s,'
+                                    sql_value.append(data)
+                        sql_list.append([sql_newline[:-1] + ')', sql_value])
+                        row += 1
+                    cursor.execute(sql_create[:-1] + ')')
+                    for list1 in sql_list:
+                        if len(list1[1]) > 0:
+                            cursor.execute(list1[0], list1[1])
+                    connection.commit()
+            except Error as e:
+                print(e)
+            finally:
+                if (connection.is_connected()):
+                    cursor.close()
+                    connection.close()
+        print("Successful exported data from " + form.lineEdit.text() + " to MySQL DataBase!")
 
 
 form.continueButton.clicked.connect(continue_click)
