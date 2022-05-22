29dc5e55ecc518e8d201255c0147e6a7f6e0cb17
Лаптев Андрей Михайлович <amlaptev@edu.hse.ru>
2022-05-22 20:08:37 +0300

    updated ui and updated code for class

--- main.py
+++ main.py
@@ -1,164 +1,213 @@
 import requests
 import mysql.connector
 import sqlite3
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-import os
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
 import pandas as pd
 
 from mysql.connector import Error
-
-Form, Window = uic.loadUiType("gui.ui")
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-
-# https://catalog.data.gov/api/3/action/package_show?id=cms-lidar-data-for-forested-areas-in-paragominas-para-brazil-2012-2014
-# https://catalog.data.gov/api/3/action/package_show?id=active-control-of-tailored-laminates
-# https://catalog.data.gov/api/3/action/package_show?id=water-quality-total-organic-carbon-removal
 
 def start_click():
     print("click start")
 
     # form.startButton.clicked.connect(start_click)
 
-
 def normilize_url(url_name):
     url_name = url_name.split('/')[3]
     
     return 'https://catalog.data.gov/api/3/action/package_show?id=' + url_name
 
-def continue_click():
-    global cursor
-    try:
-        file_name = form.lineEdit_2.text()
-        url = normilize_url(form.lineEdit.text())
-        name_file = file_name + form.comboBox.currentText()
-        response = requests.get(url)
+class Example(QWidget):
 
-        if response.status_code != 200:
-            raise Exception("status_code not equal 200!")
-        response = response.json()
-        if form.comboBox.currentText() == '.xlsx':
-            data_frame = pd.json_normalize(response['result'])
-            data_frame.to_excel(name_file)
-        print(
-            "Successful exported data from " +
-            form.lineEdit.text() +
-            " to Excel .xlsx file!")
-        if form.comboBox.currentText() == ".csv":
-            data_frame = pd.json_normalize(response['result'])
-            data_frame.to_csv(name_file)
-        print(
-            "Successful exported data from " +
-            form.lineEdit.text() +
-            " to CSV .csv file!")
-    except Exception as err:
-        print(err)
+    def __init__(self):
+        QWidget.__init__(self)
 
+        self.formats = "hfeiuaibufbuawfbwa"
 
-if form.comboBox.currentText() == "MySQL":
+        # url 
+        self.url_label = QLabel("URL", self)
+        self.url_label.setAlignment(Qt.AlignCenter)
+        self.url_text = QLineEdit(self)
+        self.url_text.setAlignment(Qt.AlignCenter)
+        
+        # file name
+        self.file_label = QLabel("File name", self)
+        self.file_label.setAlignment(Qt.AlignCenter)
+        self.file_text = QLineEdit(self)
+        self.file_text.setAlignment(Qt.AlignCenter)
 
-    mysql_host = "localhost"
-    mysql_database = "test"
-    mysql_port = 3306
-    mysql_username = "root"
-    mysql_password = "adminadmin"
-    mysql_table = "result"
-    connection = None
-    try:
-        if form.comboBox.currentText() == "MySQL":
-            connection = mysql.connector.connect(
-                host=mysql_host,
-                port=mysql_port,
-                database=mysql_database,
-                user=mysql_username,
-                password=mysql_password)
-        if connection.is_connected():
-            cursor = connection.cursor()
-            sql_delete = 'DROP TABLE IF EXISTS ' + mysql_table
-            cursor.execute(sql_delete)
+        # Availible formats
+        self.formats_label = QLabel("Availible formats", self)
+        self.formats_label.setAlignment(Qt.AlignCenter)
+        self.formats_text = QLabel(self.formats)
+        self.formats_text.setAlignment(Qt.AlignCenter)
 
-            row = 0
-            count_args = 0
-            sql_create = 'CREATE TABLE ' + mysql_table + ' ('
-            sql_list = []
-            for items in response['result']['results']:
-                sql_newline = 'INSERT ' + mysql_table + ' VALUES ('
-                sql_value = []
-                for name, data in items.items():
-                    if not isinstance(
-                            data, list) and not isinstance(
-                            data, dict) and data is not None:
-                        if row == 0:
-                            sql_create += name + ' TEXT,'
-                            count_args += 1
-                        else:
-                            sql_newline += '%s,'
-                            sql_value.append(data)
-                sql_list.append([sql_newline[:-1] + ')', sql_value])
-                row += 1
-            cursor.execute(sql_create[:-1] + ')')
-            for list1 in sql_list:
-                if len(list1[1]) > 0:
-                    cursor.execute(list1[0], list1[1])
-            connection.commit()
-    except Error as e:
-        print(e)
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-    print(
-        "Successful exported data from " +
-        form.lineEdit.text() +
-        " to MySQL DataBase!")
-if form.comboBox.currentText() == "SQLite (.db)":
-    connection = None
-    try:
-        connection = sqlite3.connect("database.db")
-        sql_table = 'database'
-        if connection:
-            cursor = connection.cursor()
-            sql_delete = 'DROP TABLE IF EXISTS ' + sql_table
-            cursor.execute(sql_delete)
+        # avaliable list
+        self.list_label = QLabel("Save as", self)
+        self.list_label.setAlignment(Qt.AlignCenter)
+        self.available_list = [".xlsx", ".csv", "MySQL", "SQLite (.db)"]
+        self.combo_box = QComboBox(self)
+        self.combo_box.addItems(self.available_list)
 
-            row = 0
-            count_args = 0
-            sql_create = 'CREATE TABLE ' + sql_table + ' ('
-            sql_list = []
-            for items in response['result']['results']:
-                sql_newline = 'INSERT INTO ' + sql_table + ' VALUES ('
-                sql_value = []
-                for name, data in items.items():
-                    if not isinstance(
-                            data, list) and not isinstance(
-                            data, dict) and data is not None:
-                        if row == 0:
-                            sql_create += name + ' TEXT,'
-                            count_args += 1
-                        else:
-                            sql_newline += '?,'
-                            sql_value.append(data)
-                sql_list.append([sql_newline[:-1] + ')', sql_value])
-                row += 1
-            cursor.execute(sql_create[:-1] + ')')
-            for list1 in sql_list:
-                if len(list1[1]) > 0:
-                    cursor.execute(list1[0], list1[1])
-            connection.commit()
-    except Error as e:
-        print(e)
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-    print(
-        "Successful exported data from " +
-        form.lineEdit.text() +
-        " to SQLite DataBase!")
+        self.btn_submit = QPushButton("Continue", self)
 
-form.continueButton.clicked.connect(continue_click)
-app.exec_()
+        # all includes
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.url_label, 0, 0)
+        self.layout.addWidget(self.url_text, 0, 1)
+        self.layout.addWidget(self.file_label, 2, 0)
+        self.layout.addWidget(self.file_text, 2, 1)
+        self.layout.addWidget(self.formats_label, 4, 0)
+        self.layout.addWidget(self.formats_text, 4, 1)
+        self.layout.addWidget(self.list_label, 6, 0)
+        self.layout.addWidget(self.combo_box, 6, 1)
+        self.layout.addWidget(self.btn_submit, 10, 1)
+
+        self.setLayout(self.layout)
+        self.show()
+
+        self.btn_submit.pressed.connect(self.continue_click)
+
+    def continue_click(self):
+        global cursor
+        try:
+            file_name = self.file_text.text()
+            url = normilize_url(self.url_text.text())
+            name_file = file_name + self.combo_box.currentText()
+            response = requests.get(url)
+
+            if response.status_code != 200:
+                raise Exception("status_code not equal 200!")
+
+            response = response.json()
+
+            if self.combo_box.currentText() == '.xlsx':
+                data_frame = pd.json_normalize(response['result'])
+                data_frame.to_excel(name_file)
+                print("Successful exported data from " + self.file_text.text() + " to Excel .xlsx file!")
+
+            if self.combo_box.currentText() == ".csv":
+                data_frame = pd.json_normalize(response['result'])
+                data_frame.to_csv(name_file)
+                print("Successful exported data from " + self.file_text.text() + " to CSV .csv file!")
+
+            if self.combo_box.currentText() == "MySQL":
+                mysql_host = "localhost"
+                mysql_database = "test"
+                mysql_port = 3306
+                mysql_username = "root"
+                mysql_password = "adminadmin"
+                mysql_table = "result"
+                connection = None
+                
+                try:
+                    if self.combo_box.currentText() == "MySQL":
+                        connection = mysql.connector.connect(
+                            host=mysql_host,
+                            port=mysql_port,
+                            database=mysql_database,
+                            user=mysql_username,
+                            password=mysql_password)
+
+                    if connection.is_connected():
+                        cursor = connection.cursor()
+                        sql_delete = 'DROP TABLE IF EXISTS ' + mysql_table
+                        cursor.execute(sql_delete)
+
+                        row = 0
+                        count_args = 0
+                        sql_create = 'CREATE TABLE ' + mysql_table + ' ('
+                        sql_list = []
+
+                        for items in response['result']['results']:
+                            sql_newline = 'INSERT ' + mysql_table + ' VALUES ('
+                            sql_value = []
+
+                            for name, data in items.items():
+                                if not isinstance(
+                                        data, list) and not isinstance(
+                                        data, dict) and data is not None:
+                                    if row == 0:
+                                        sql_create += name + ' TEXT,'
+                                        count_args += 1
+                                    else:
+                                        sql_newline += '%s,'
+                                        sql_value.append(data)
+
+                            sql_list.append([sql_newline[:-1] + ')', sql_value])
+                            row += 1
+
+                        cursor.execute(sql_create[:-1] + ')')
+
+                        for list1 in sql_list:
+                            if len(list1[1]) > 0:
+                                cursor.execute(list1[0], list1[1])
+
+                        connection.commit()
+                except Error as e:
+                    print(e)
+                finally:
+                    if connection.is_connected():
+                        cursor.close()
+                        connection.close()
+                print("Successful exported data from " + self.file_text.text() + " to MySQL DataBase!")
+
+            if self.combo_box.currentText() == "SQLite (.db)":
+                connection = None
+                try:
+                    connection = sqlite3.connect("database.db")
+                    sql_table = 'database'
+
+                    if connection:
+                        cursor = connection.cursor()
+                        sql_delete = 'DROP TABLE IF EXISTS ' + sql_table
+                        cursor.execute(sql_delete)
+
+                        row = 0
+                        count_args = 0
+                        sql_create = 'CREATE TABLE ' + sql_table + ' ('
+                        sql_list = []
+
+                        for items in response['result']['results']:
+                            sql_newline = 'INSERT INTO ' + sql_table + ' VALUES ('
+                            sql_value = []
+
+                            for name, data in items.items():
+                                if not isinstance(data, list) and not isinstance(data, dict) and data is not None:
+                                    if row == 0:
+                                        sql_create += name + ' TEXT,'
+                                        count_args += 1
+                                    else:
+                                        sql_newline += '?,'
+                                        sql_value.append(data)
+
+                            sql_list.append([sql_newline[:-1] + ')', sql_value])
+                            row += 1
+
+                        cursor.execute(sql_create[:-1] + ')')
+
+                        for list1 in sql_list:
+                            if len(list1[1]) > 0:
+                                cursor.execute(list1[0], list1[1])
+
+                        connection.commit()
+                except Error as e:
+                    print(e)
+                
+                finally:
+                    if connection:
+                        cursor.close()
+                        connection.close()
+
+                print("Successful exported data from " + self.file_text.text() + " to SQLite DataBase!")
+
+        except Exception as err:
+            print(err)
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
