cdd8d3ef8e5187ed5dc38a53ecd03facce451a5b
KryzPetr1533 <62902540+KryzPetr1533@users.noreply.github.com>
2022-05-22 17:34:25 +0300

    Uploaded my part of code to the branch for following merge

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
 import requests
-import xlsxwriter
+import mysql.connector
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+import os
+import pandas as pd
 
-name = 'file'
-# Create a workbook (exel file) and add a worksheet (table).
-workbook = xlsxwriter.Workbook(name + '.xlsx')
-worksheet = workbook.add_worksheet()
+from mysql.connector import Error
 
-# Some data (this is test version) we want to write to the worksheet.
-response = requests.get('http://catalog.data.gov/api/3/action/package_list')
-response = response.json()
+Form, Window = uic.loadUiType("gui.ui")
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
 
-# print(*response.items(), sep = '\n')
-# Start from the first cell. Rows and columns are zero indexed.
-row = 0
-col = 0
-width = 20
+# https://catalog.data.gov/api/3/action/package_show?id=cms-lidar-data-for-forested-areas-in-paragominas-para-brazil-2012-2014
+# https://catalog.data.gov/api/3/action/package_show?id=active-control-of-tailored-laminates
+# https://catalog.data.gov/api/3/action/package_show?id=water-quality-total-organic-carbon-removal
 
-for items in response['result']['results']:
-    col = 0
-    for name, data in items.items():
-        if type(data) is not list and type(data) is not dict and data is not None:
-            if row == 0:
-                worksheet.write(row, col, name, workbook.add_format({'bold': 1}))
-            else:
-                worksheet.write(row, col, data)
-            # Adjust the column width.
-            worksheet.set_column(row, col, width)
-            col += 1
-    row += 1
+def start_click():
+    print("click start")
 
-# Iterate over the data and write it out row by row.
+    # form.startButton.clicked.connect(start_click)
 
-# # Write a total using a formula.
-# worksheet.write(row, 0, 'Total')
-# worksheet.write(row, 1, '=SUM(B1:B4)')
 
-workbook.close()
+def continue_click():
+    global cursor
+    url = form.lineEdit.text()
+    name_file = 'file' + form.comboBox.currentText()
+    # Some data (this is test version) we want to write to save.
+    response = requests.get(url)
+    response = response.json()
+    if form.comboBox.currentText() == '.xlsx':
+        data_frame = pd.json_normalize(response['result'])
+        data_frame.to_excel(name_file)
+    print(
+        "Successful exported data from " +
+        form.lineEdit.text() +
+        " to Excel .xlsx file!")
+    if form.comboBox.currentText() == ".csv":
+        data_frame = pd.json_normalize(response['result'])
+        data_frame.to_csv(name_file)
+    print(
+        "Successful exported data from " +
+        form.lineEdit.text() +
+        " to CSV .csv file!")
+
+form.continueButton.clicked.connect(continue_click)
+app.exec_()
